668449ccced53e199b48429b8c408c94ff5cad82
Chris Young <ctopheryoung@gmail.com>
2016-04-22 12:51:34 -0700

    Create Empty SignUp and Login Activities

--- app/src/main/java/xyz/chrisyoung/foos/MainActivity.java
+++ app/src/main/java/xyz/chrisyoung/foos/MainActivity.java
@@ -1,13 +1,35 @@
 package xyz.chrisyoung.foos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.signUpButton) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mLoginButton.setOnClickListener(this);
+        mSignUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginButton) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        } if (v == mSignUpButton) {
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        }
     }
 }
