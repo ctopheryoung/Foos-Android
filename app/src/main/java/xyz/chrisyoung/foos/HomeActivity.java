c8d13e243e5f4d2f364c7e19f50a8e8a3cb2f0eb
Chris Young <ctopheryoung@gmail.com>
2016-04-24 20:40:59 -0700

    Add "Home" activity with leaderboard listview and takes intent/extra from login screen

--- app/src/main/java/xyz/chrisyoung/foos/HomeActivity.java
+++ app/src/main/java/xyz/chrisyoung/foos/HomeActivity.java
@@ -1,13 +1,32 @@
 package xyz.chrisyoung.foos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+    @Bind(R.id.welcomeTextView) TextView mWelcomeTextView;
+    @Bind(R.id.leaderboard) ListView mLeaderboard;
+
+    public String[] players = new String[] {"1. Summer", "2. Cooper", "3. Sarah", "4. Eric", "5. Chris", "6. Ben", "7. Emily", "8. Mike", "9. Matt", "10. Nolan", "11. Steve", "12. Michelle", "13. Mary", "14. Ramone", "15. Trevor"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, players);
+        mLeaderboard.setAdapter(adapter);
+
+        Intent intent = getIntent();
+        String userEmail = intent.getStringExtra("userEmail");
+        mWelcomeTextView.setText("Welcome, " + userEmail);
     }
 }
--- app/src/main/java/xyz/chrisyoung/foos/LoginActivity.java
+++ app/src/main/java/xyz/chrisyoung/foos/LoginActivity.java
@@ -1,13 +1,35 @@
 package xyz.chrisyoung.foos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.loginButton) TextView mLoginButton;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginButton) {
+            String userEmail = mEmailEditText.getText().toString();
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            intent.putExtra("userEmail", userEmail);
+            startActivity(intent);
+        }
     }
 }
