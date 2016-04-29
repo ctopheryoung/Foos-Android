166d68e2035ec7f5fe8cab29a10daa6af3f3d835
Chris <ctopheryoung@gmail.com>
2016-04-28 17:06:29 -0700

    create initial User model

---
+++ app/src/main/java/xyz/chrisyoung/foos/models/User.java
@@ -0,0 +1,62 @@
+package xyz.chrisyoung.foos.models;
+
+/**
+ * Created by Guest on 4/28/16.
+ */
+public class User {
+    private int mId;
+    private String mFirstName;
+    private String mLastName;
+    private String mEmail;
+    private int mGameCount = 0;
+
+    public User (String firstName, String lastName, String email) {
+        mFirstName = firstName;
+        mLastName = lastName;
+        mEmail = email;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String mFirstName) {
+        this.mFirstName = mFirstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String mLastName) {
+        this.mLastName = mLastName;
+    }
+
+    public String getFullName() {
+        return mFirstName + " " + mLastName;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public int getGameCount() {
+        return mGameCount;
+    }
+
+    public void setGameCount(int mGameCount) {
+        this.mGameCount = mGameCount;
+    }
+}
