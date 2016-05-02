48a52cc11d138c0f08c0ea541a5a16167fcc2920
Chris Young <ctopheryoung@gmail.com>
2016-05-01 21:53:35 -0700

    add initial MyGames fragment logic

---
+++ app/src/main/java/xyz/chrisyoung/foos/ui/MyGamesFragment.java
@@ -0,0 +1,53 @@
+package xyz.chrisyoung.foos.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import xyz.chrisyoung.foos.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyGamesFragment extends Fragment {
+    // Store instance variables
+    private String title;
+    private int page;
+
+    public MyGamesFragment() {
+        // Required empty public constructor
+    }
+
+
+    // newInstance constructor for creating fragment with arguments
+    public static MyGamesFragment newInstance(int page, String title) {
+        MyGamesFragment fragmentMyGames = new MyGamesFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragmentMyGames.setArguments(args);
+        return fragmentMyGames;
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_games, container, false);
+//        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
+//        tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
