150d47bcd9f87d5a2f35ef51d0acf728e77aed5e
Chris Young <ctopheryoung@gmail.com>
2016-05-03 23:06:31 -0700

    remove id and gamecount member variables (game count can be computed from wins + losses)

--- app/src/main/java/xyz/chrisyoung/foos/models/Game.java
+++ app/src/main/java/xyz/chrisyoung/foos/models/Game.java
@@ -1,7 +1,65 @@
 package xyz.chrisyoung.foos.models;
 
+import java.util.Date;
+
 /**
  * Created by Guest on 4/28/16.
  */
 public class Game {
+    private String winner;
+    private String loser;
+    private int winnerScore;
+    private int loserScore;
+    private Date timeStamp;
+
+    public Game() {}
+
+    public Game(String winner, String loser, int winnerScore, int loserScore) {
+        this.winner = winner;
+        this.loser = loser;
+        this.winnerScore = winnerScore;
+        this.loserScore = loserScore;
+        this.timeStamp = new Date();
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public String getLoser() {
+        return loser;
+    }
+
+    public void setLoser(String loser) {
+        this.loser = loser;
+    }
+
+    public int getWinnerScore() {
+        return winnerScore;
+    }
+
+    public void setWinnerScore(int winnerScore) {
+        this.winnerScore = winnerScore;
+    }
+
+    public int getLoserScore() {
+        return loserScore;
+    }
+
+    public void setLoserScore(int loserScore) {
+        this.loserScore = loserScore;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
+
