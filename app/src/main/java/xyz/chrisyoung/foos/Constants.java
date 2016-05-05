5f260b613fad6cba611f15c881d703380918a12d
ctopheryoung <ctopheryoung@gmail.com>
2016-05-05 15:43:21 -0700

    add constants refs to firebase nodes

--- app/src/main/java/xyz/chrisyoung/foos/Constants.java
+++ app/src/main/java/xyz/chrisyoung/foos/Constants.java
@@ -7,7 +7,13 @@ public class Constants {
     public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
 
     public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_LOCATION_GAMES = "games";
+    public static final String FIREBASE_LOCATION_RATINGS = "ratings";
+
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
+    public static final String FIREBASE_URL_GAMES = FIREBASE_URL + "/" + FIREBASE_LOCATION_GAMES;
+    public static final String FIREBASE_URL_RATINGS = FIREBASE_URL + "/" + FIREBASE_LOCATION_RATINGS;
+
     public static final String FIREBASE_PROPERTY_EMAIL = "email";
     public static final String KEY_UID = "UID";
-    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
 }
