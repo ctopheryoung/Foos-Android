9e39dec889bc4ad7f5bfef19fc38514d8ca82087
Chris Young <ctopheryoung@gmail.com>
2016-05-13 11:37:07 -0700

    setup winner spinner to display list of all user full names

--- app/src/main/java/xyz/chrisyoung/foos/ui/RecordGameActivity.java
+++ app/src/main/java/xyz/chrisyoung/foos/ui/RecordGameActivity.java
@@ -7,41 +7,76 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import xyz.chrisyoung.foos.Constants;
 import xyz.chrisyoung.foos.R;
 import xyz.chrisyoung.foos.models.Game;
+import xyz.chrisyoung.foos.models.User;
 
 public class RecordGameActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = RecordGameActivity.class.getSimpleName();
     private SharedPreferences mSharedPreferences;
+    private Firebase mFirebaseUsersRef;
+    public ArrayList<String> mUsers = new ArrayList<>();
 
-    @Bind(R.id.winnerSpinner) EditText mWinnerSpinner;
     @Bind(R.id.winnerScoreEditText) EditText mWinnerScoreEditText;
     @Bind(R.id.loserSpinner) EditText mLoserSpinner;
     @Bind(R.id.loserScoreEditText) EditText mLoserScoreEditText;
     @Bind(R.id.saveButton) Button mSaveButton;
+    @Bind(R.id.winnerSpinner) Spinner mWinnerSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_game);
         ButterKnife.bind(this);
+        mFirebaseUsersRef = new Firebase(Constants.FIREBASE_URL_USERS);
         mSaveButton.setOnClickListener(this);
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        setUpWinnerSpinner();
+        setUpLoserSpinner();
+    }
+
+    private void setUpWinnerSpinner(){
+        mFirebaseUsersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mUsers.clear();
+                for(DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    mUsers.add(postSnapshot.child("fullName").getValue().toString());
+                }
+                ArrayAdapter adapter = new ArrayAdapter(RecordGameActivity.this, android.R.layout.simple_spinner_item, mUsers);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                mWinnerSpinner.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+    }
+
+    private void setUpLoserSpinner(){
+
     }
 
     @Override
     public void onClick(View view) {
         if (view == mSaveButton) {
-            String winner = mWinnerSpinner.getText().toString();
+            String winner = mWinnerSpinner.getSelectedItem().toString();
             String loser = mLoserSpinner.getText().toString();
             Integer winnerScore = Integer.parseInt(mWinnerScoreEditText.getText().toString());
             Integer loserScore = Integer.parseInt(mLoserScoreEditText.getText().toString());
