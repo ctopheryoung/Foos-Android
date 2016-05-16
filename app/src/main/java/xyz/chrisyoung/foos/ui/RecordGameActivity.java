9fb1e4fb765c2086099147a49931cca4ca3d3e6f
Chris Young <ctopheryoung@gmail.com>
2016-05-15 17:23:50 -0700

    change spinners to display entire user object instead of just name or ID

--- app/src/main/java/xyz/chrisyoung/foos/ui/RecordGameActivity.java
+++ app/src/main/java/xyz/chrisyoung/foos/ui/RecordGameActivity.java
@@ -11,18 +11,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
-
 import java.util.ArrayList;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import jskills.GameInfo;
+import jskills.Player;
+import jskills.Team;
 import xyz.chrisyoung.foos.Constants;
 import xyz.chrisyoung.foos.R;
 import xyz.chrisyoung.foos.models.Game;
@@ -34,12 +34,9 @@ public class RecordGameActivity extends AppCompatActivity implements View.OnClic
     private Firebase mFirebaseUsersRef;
     private Query mWinnerQuery;
     private String mWinnerId;
-    private ValueEventListener mWinnerIdRefListener;
-    private ValueEventListener mLoserIdRefListener;
     private String mLoserId;
     private Query mLoserQuery;
-    private ValueEventListener mUserIDRefListener;
-    public ArrayList<String> mUsers = new ArrayList<>();
+    public ArrayList<User> mUsers = new ArrayList<>();
 
     @Bind(R.id.winnerScoreEditText) EditText mWinnerScoreEditText;
     @Bind(R.id.loserScoreEditText) EditText mLoserScoreEditText;
@@ -64,7 +61,7 @@ public class RecordGameActivity extends AppCompatActivity implements View.OnClic
             public void onDataChange(DataSnapshot dataSnapshot) {
                 mUsers.clear();
                 for(DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-                    mUsers.add(postSnapshot.child("fullName").getValue().toString());
+                    mUsers.add(postSnapshot.getValue(User.class));
                 }
                 ArrayAdapter adapter = new ArrayAdapter(RecordGameActivity.this, android.R.layout.simple_spinner_item, mUsers);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -82,80 +79,70 @@ public class RecordGameActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onClick(View view) {
         if (view == mSaveButton) {
-            String winnerName = mWinnerSpinner.getSelectedItem().toString();
-            String loserName = mLoserSpinner.getSelectedItem().toString();
-
-            mWinnerQuery = mFirebaseUsersRef.orderByChild("fullName").equalTo(winnerName);
-
-            mWinnerQuery.addChildEventListener(new ChildEventListener() {
-                @Override
-                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                    mWinnerId=dataSnapshot.getKey();
-                    Log.d(TAG, "Winner ID in datasnapshot: "+mWinnerId);
-                }
-
-                @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onCancelled(FirebaseError firebaseError) {
-
-                }
-            });
-
+            User winner = (User)mWinnerSpinner.getSelectedItem();
+            User loser = (User)mLoserSpinner.getSelectedItem();
             Integer winnerScore = Integer.parseInt(mWinnerScoreEditText.getText().toString());
             Integer loserScore = Integer.parseInt(mLoserScoreEditText.getText().toString());
-            String userId = mSharedPreferences.getString(Constants.KEY_UID, null);
+            String currentUserId = mSharedPreferences.getString(Constants.KEY_UID, null);
+
+// This was my attempt at pulling user Ids out of Firebase when I had names in the Spinners.
+//            mWinnerQuery = mFirebaseUsersRef.orderByChild("fullName").equalTo(winnerName);
+//
+//            mWinnerQuery.addChildEventListener(new ChildEventListener() {
+//                @Override
+//                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                    mWinnerId=dataSnapshot.getKey();
+//                    Log.d(TAG, "mWinnerID in Event Listener: " + mWinnerId);
+//                }
+//
+//                @Override
+//                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//                }
+//
+//                @Override
+//                public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//                }
+//
+//                @Override
+//                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//                }
+//
+//                @Override
+//                public void onCancelled(FirebaseError firebaseError) {
+//
+//                }
+//            });
 
             // 1. Find both players
             // 2. Find each players rating
             // 3. Calculate new ratings
             // 4. Update ratings and calculate new TrueSkill -> playerObject.updateRating(newMean, newStdDev)
-            try {
-                Thread.sleep(500);
-                Log.d(TAG, "Sleep success.");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Log.d(TAG, "Sleep problems");
-            }
-            Log.d(TAG, "Winner ID just before newGame object creation"+mWinnerId);
-            Game newGame = new Game(mWinnerId, mLoserId, winnerScore, loserScore, userId);
-            Log.d(TAG, "getWinner of newGame object="+newGame.getWinner());
-//            saveGameToFirebase(newGame);
+
+            Player<User> playerWinner = new Player<>(winner);
+            Player<User> playerLoser = new Player<>(loser);
+            GameInfo gameInfo = GameInfo.getDefaultGameInfo();
+
+            Game newGame = new Game(winner.getFullName(), winner.getPushId(), loser.getFullName(), loser.getPushId(), winnerScore, loserScore, currentUserId);
+            saveGameToFirebase(newGame);
+//            updateWinnerScore();
+//            updateLoserScore();
         }
     }
 
     public void saveGameToFirebase(Game game) {
         //Saves game and single entity in "games" child node
         Firebase gamesRef = new Firebase(Constants.FIREBASE_URL_GAMES);
-        gamesRef.push().setValue(game);
+        Firebase newGameRef = gamesRef.push();
+        String pushId = newGameRef.getKey();
+        game.setPushId(pushId);
+        newGameRef.setValue(game);
 
         //Saves game in playerGames child once for each player (with under nodes with winnerID and loserID)
         Firebase playerGamesRef = new Firebase(Constants.FIREBASE_URL_PLAYER_GAMES);
-        Firebase newGameRef = playerGamesRef.push();
-        String pushId = newGameRef.getKey();
-        game.setPushId(pushId);
-        playerGamesRef.child(game.getWinner()).child(pushId).setValue(game);
-        playerGamesRef.child(game.getLoser()).child(pushId).setValue(game);
+        playerGamesRef.child(game.getWinnerId()).child(pushId).setValue(game);
+        playerGamesRef.child(game.getLoserId()).child(pushId).setValue(game);
     }
-
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        mWinnerQuery.removeEventListener(mUserIDRefListener);
-//        mLoserQuery.removeEventListener(mLoserIdRefListener);
-//    }
-
 }
