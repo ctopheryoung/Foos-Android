b24e9af15f81292a108f1448b93f6bc907e2416a
Chris Young <ctopheryoung@gmail.com>
2016-06-22 19:35:06 -0700

    Add auth state listener to startup activity, takes user to account in signed in

--- app/src/main/java/xyz/chrisyoung/foos/ui/MainActivity.java
+++ app/src/main/java/xyz/chrisyoung/foos/ui/MainActivity.java
@@ -1,19 +1,27 @@
 package xyz.chrisyoung.foos.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import xyz.chrisyoung.foos.R;
+import xyz.chrisyoung.foos.util.Constants;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.loginButton) TextView mLoginButton;
     @Bind(R.id.signUpButton) TextView mSignUpButton;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "fonts/bebasneue.ttf");
         mLoginButton.setTypeface(myCustomFont);
         mSignUpButton.setTypeface(myCustomFont);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    mSharedPreferencesEditor.putString(Constants.KEY_UID, user.getUid()).apply();
+                    // User is signed in
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
     }
 
     @Override
